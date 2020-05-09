c066825b249ee24661dfdf61c17a037afbbc8879
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-05-09 15:40:38 -0400

    First commit with DATS schedule

---
+++ js/schedule.js
@@ -0,0 +1,224 @@
+﻿{
+
+	"sessions": [
+
+		{
+			"title": "Introduction",
+			"speaker": "Dev Around the Sun Team",
+			"start": "2020-05-12T12:00:00.000Z"
+		},
+		{
+			"title": "Static generated sites === great performance. What are you waiting for?",
+			"speaker": "Debbie O'Brien",
+			"start": "2020-05-12T12:10:00.000Z"
+		},
+		{
+			"title": "Stack Overflow Podcast",
+			"speaker": "Stack Overflow Team",
+			"start": "2020-05-12T12:45:00.000Z"
+		},
+		{
+			"title": "Bridge to Newthingia: How to answer 'why will yours succeed, when X, Y, and Z have failed?'",
+			"speaker": "Herb Sutter",
+			"start": "2020-05-12T13:20:00.000Z"
+		},
+		{
+			"title": "The Evolution of Computing",
+			"speaker": "Mark Rendle",
+			"start": "2020-05-12T13:55:00.000Z"
+		},
+		{
+			"title": "When you think there is no time for learning or coding",
+			"speaker": "Eleftheria Batsou",
+			"start": "2020-05-12T14:30:00.000Z"
+		},
+		{
+			"title": "Manipulating Webpages using DevTools (for a better web experience)",
+			"speaker": "Jemima Abu",
+			"start": "2020-05-12T15:05:00.000Z"
+		},
+		{
+			"title": "Wibbly-Wobbly, Timey-Wimey Stuff",
+			"speaker": "Kevlin Henney",
+			"start": "2020-05-12T15:40:00.000Z"
+		},
+		{
+			"title": "Social distancing and technology",
+			"speaker": "Nenne Adaora (Adora) Nwodo",
+			"start": "2020-05-12T16:15:00.000Z"
+		},
+		{
+			"title": "Q&A with DHH",
+			"speaker": "David Heinemeier Hansson",
+			"start": "2020-05-12T16:50:00.000Z"
+		},
+		{
+			"title": "Making a Shiny Linux Environment in Windows",
+			"speaker": "Scott Hanselman",
+			"start": "2020-05-12T17:25:00.000Z"
+		},
+		{
+			"title": "",
+			"speaker": "",
+			"start": ""
+		},
+		{
+			"title": "Battling Ants in Your Pants - If You're Wearing Any",
+			"speaker": "Sanja Bonic",
+			"start": "2020-05-12T18:35:00.000Z"
+		},
+		{
+			"title": "A playful approach to domain language",
+			"speaker": "Marco Heimeshoff",
+			"start": "2020-05-12T19:10:00.000Z"
+		},
+		{
+			"title": "Watching a Leopard change its spots; Microsoft and Open Source",
+			"speaker": "Danese Cooper",
+			"start": "2020-05-12T19:45:00.000Z"
+		},
+		{
+			"title": "Colonizing Venus",
+			"speaker": "Richard Campbell",
+			"start": "2020-05-12T20:20:00.000Z"
+		},
+		{
+			"title": "I Spent Millions On Video Game Development So You Don't Have To",
+			"speaker": "Heather Wilde",
+			"start": "2020-05-12T20:55:00.000Z"
+		},
+		{
+			"title": "Low-Context DevOps: Helping DevOps find answers faster",
+			"speaker": "Thomas Limoncelli",
+			"start": "2020-05-12T21:30:00.000Z"
+		},
+		{
+			"title": "Essential Tips for Building Developer Libraries",
+			"speaker": "Laura Rodriguez",
+			"start": "2020-05-12T22:05:00.000Z"
+		},
+		{
+			"title": "A Few Ways to Improve Code Quality",
+			"speaker": "Venkat Subramaniam",
+			"start": "2020-05-12T22:40:00.000Z"
+		},
+		{
+			"title": "How NOT to Restore a VW Bus",
+			"speaker": "Matt Raible",
+			"start": "2020-05-12T23:15:00.000Z"
+		},
+		{
+			"title": "What C++ is and Why?",
+			"speaker": "Bjarne Stroustrup",
+			"start": "2020-05-12T23:50:00.000Z"
+		},
+		{
+			"title": "What do developers build? We build change",
+			"speaker": "Bill Wagner",
+			"start": "2020-05-13T00:25:00.000Z"
+		},
+		{
+			"title": "5 Ways Moving to a Farm in the Middle of Nowhere Has Made Me a Better Developer",
+			"speaker": "Todd Sharp",
+			"start": "2020-05-13T01:00:00.000Z"
+		},
+		{
+			"title": "A Banjo Made Me a Better Developer",
+			"speaker": "Jeremy Clark",
+			"start": "2020-05-13T01:35:00.000Z"
+		},
+		{
+			"title": "Unlock Your Potential",
+			"speaker": "David Neal",
+			"start": "2020-05-13T02:10:00.000Z"
+		},
+		{
+			"title": "Our data, our databases and AI: Elements of a better future",
+			"speaker": "Dani Monteiro",
+			"start": "2020-05-13T02:45:00.000Z"
+		},
+		{
+			"title": "How Machine Learning Learns",
+			"speaker": "Nick Larsen",
+			"start": "2020-05-13T03:20:00.000Z"
+		},
+		{
+			"title": "Granny was a Hacker",
+			"speaker": "Kris Howard",
+			"start": "2020-05-13T03:55:00.000Z"
+		},
+		{
+			"title": "Breach Around the Sun",
+			"speaker": "Troy Hunt",
+			"start": "2020-05-13T04:30:00.000Z"
+		},
+		{
+			"title": "Hey, where is my country? Make all your end users happy.",
+			"speaker": "Renato Losio",
+			"start": "2020-05-13T05:05:00.000Z"
+		},
+		{
+			"title": "Fear of shipping and the last mile problem",
+			"speaker": "Leena S N",
+			"start": "2020-05-13T05:40:00.000Z"
+		},
+		{
+			"title": "Data is Obnoxiously Pleasant - SQL as Calculator",
+			"speaker": "Pinal Dave",
+			"start": "2020-05-13T06:15:00.000Z"
+		},
+		{
+			"title": "Beer and IoT is a never ending story.",
+			"speaker": "Samuel Adranyi",
+			"start": "2020-05-13T06:50:00.000Z"
+		},
+		{
+			"title": "Life set No Limits Mode On & Covid19 Initiatives WorldWide",
+			"speaker": "Viviane Ribeiro",
+			"start": "2020-05-13T07:25:00.000Z"
+		},
+		{
+			"title": "Best Practices for writing a remote friendly resume",
+			"speaker": "Edem Kumodzi",
+			"start": "2020-05-13T08:00:00.000Z"
+		},
+		{
+			"title": "Machine Learning in 25min",
+			"speaker": "Heli Helskyaho",
+			"start": "2020-05-13T08:35:00.000Z"
+		},
+		{
+			"title": "A Love Letter To HTML",
+			"speaker": "Lemon 🍋",
+			"start": "2020-05-13T09:10:00.000Z"
+		},
+		{
+			"title": "Democratising Technology Education",
+			"speaker": "Nyari Samushonga",
+			"start": "2020-05-13T09:45:00.000Z"
+		},
+		{
+			"title": "Simplifying JavaScript for Every Developer",
+			"speaker": "Dhananjay Kumar",
+			"start": "2020-05-13T10:20:00.000Z"
+		},
+		{
+			"title": "I don't want to talk to you. Can I talk to the chatbot?",
+			"speaker": "Patricio Cofre",
+			"start": "2020-05-13T10:55:00.000Z"
+		},
+		{
+			"title": "Dates, time zones and calendar systems - oh my!",
+			"speaker": "Jon Skeet",
+			"start": "2020-05-13T11:30:00.000Z"
+		},
+		{
+			"title": "Wrap Up and Thank You!",
+			"speaker": "Dev Around the Sun Team",
+			"start": "2020-05-13T11:55:00.000Z"
+		}
+
+
+	]
+
+}
---
+++ js/site.js
@@ -0,0 +1,138 @@
+﻿(function () {
+
+	var widget = {
+
+		timerThread: {},
+
+		displayCount: 3,
+
+		schedule: {},
+
+		start: function () {
+
+			fetch("/js/schedule.js").then(response => response.text())
+				.then(response => {
+					widget.schedule = JSON.parse(response, JSON.dateParser).sessions;
+					widget.updateSchedule();
+					timerThread = window.setInterval(widget.updateSchedule, 5000);
+			});
+
+		},
+
+		sessions: [],
+
+		updateSchedule: function () {
+
+			//console.log("Analyzing time");
+			var dt = new Date();
+			for (var item of widget.sessions) {
+				//console.log(`Start: ${item.start}, Now: ${dt}`)
+				if (item.start < dt) {
+					widget.sessions = [];
+					//console.log("clearing sessions");
+					break;
+				}
+			}
+
+
+			var added = false;
+			//console.log("analyzing schedule");
+			for (var item of widget.schedule) {
+
+				if (item.start > dt && widget.sessions.length < widget.displayCount) {
+				//	console.log(`Adding session ${item.title}`);
+					widget.sessions.push(item);
+					added = true;
+				}
+
+			}
+			if (added) {
+				widget.updateUI();
+			}
+
+		},
+
+		updateUI: function () {
+			var next = document.getElementById("widget");
+			if (next.children.length > 0) {
+				while (next.childNodes.length > 0) {
+					next.removeChild(next.lastChild);
+				}
+			} 
+
+			SetNow();
+
+			//console.log(`Adding items [${widget.sessions.length}]`);
+			for (var item of widget.sessions) {
+
+				AddItem(next, item);
+
+			}
+
+			function AddItem(ui, item, showTime = true) {
+
+				var el = document.createElement("p");
+				var t = document.createElement("b");
+				t.textContent = item.title
+				el.appendChild(t);
+				var s = document.createElement("span");
+				s.textContent = item.speaker
+
+				if (showTime) {
+					var u = document.createElement("u");
+					u.textContent = moment(item.start).fromNow()
+					s.appendChild(u);
+				}
+				el.appendChild(s);
+				ui.appendChild(el);
+
+			}
+
+			function SetNow() {
+
+				var n = document.getElementById("now");
+				while (n.childNodes.length > 0) {
+					n.removeChild(n.lastChild);
+				}
+
+				var dt = new Date();
+				for (var item of widget.schedule) {
+					if (item.start < dt && moment(item.start).add(25,'m').toDate() >= dt) {
+						AddItem(n, item, false);
+						return;
+					}
+				}
+
+
+			}
+		}
+
+
+	};
+
+	window.widget = widget;
+
+	// JSON date handling
+	if (window.JSON && !window.JSON.dateParser) {
+		var reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+		var reMsAjax = /^\/Date\((d|-|.*)\)[\/|\\]$/;
+
+		JSON.dateParser = function (key, value) {
+			if (typeof value === 'string') {
+				var a = reISO.exec(value);
+				if (a)
+					return new Date(value);
+				a = reMsAjax.exec(value);
+				if (a) {
+					var b = a[1].split(/[-+,.]/);
+					return new Date(b[0] ? +b[0] : 0 - +b[1]);
+				}
+			}
+			return value;
+		};
+
+	}
+
+	widget.start();
+
+})();
